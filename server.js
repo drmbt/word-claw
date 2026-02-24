ed7a89f70779e7757d2db1c54c4fde5af5f4b1bc
Vincent Naples <dreambaitrecordings@gmail.com>
2026-02-24 10:56:58 -0800

    feat: move default vocab to dedicated file, expand to 600+ words, append custom env words, listen on 0.0.0.0

--- server.js
+++ server.js
@@ -11,41 +11,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Also serve from root since we are transitioning to a backend
 app.use(express.static(__dirname));
 
-app.get('/api/config', (req, res) => {
-    // Provide a massive default vocab list if none is set in the env
-    const defaultVocab = [
-        "the", "of", "and", "a", "to", "in", "is", "you", "that", "it", "he", "was", "for", "on", "are", "as", "with", "his", "they", "I",
-        // ... (Adding a large list of words to make it interesting)
-        "love", "death", "blood", "neon", "cyber", "flesh", "machine", "ghost", "shell", "soul", "heart", "mind", "memory", "data", "system",
-        "fuck", "shit", "damn", "bitch", "cunt", "motherfucker", "asshole", "dick", "pussy", "cock", "slut", "whore", "bastard", "crap", "piss",
-        // Sexual and R-rated
-        "sex", "orgasm", "climax", "penetration", "lust", "desire", "erotic", "kink", "fetish", "bondage", "sadism", "masochism", "dominant", "submissive",
-        // Pop culture
-        "matrix", "jedi", "sith", "vader", "skywalker", "hobbit", "gandalf", "mordor", "hogwarts", "potter", "voldemort", "batman", "joker", "gotham", "superman",
-        "thanos", "avengers", "stark", "wakanda", "targaryen", "stark", "lannister", "westeros", "dragon", "terminator", "skynet", "predator", "alien", "ripley",
-        // Action verbs
-        "destroy", "kill", "murder", "smash", "break", "crush", "burn", "melt", "freeze", "shock", "electrocute", "slice", "cut", "tear", "rip",
-        // Adjectives
-        "bloody", "violent", "fucking", "goddamn", "beautiful", "gorgeous", "vile", "disgusting", "horrific", "terrifying", "amazing", "incredible", "awesome",
-        // Nouns
-        "knife", "gun", "sword", "bomb", "bullet", "laser", "plasma", "fire", "ice", "poison", "toxin", "venom", "acid", "radioactive", "nuclear",
-        // More literary/poetic
-        "whisper", "shadow", "moonlight", "starlight", "ocean", "tide", "breeze", "storm", "thunder", "lightning", "rain", "snow", "frost", "ember", "ash",
-        "sorrow", "grief", "joy", "bliss", "ecstasy", "agony", "pain", "pleasure", "laughter", "tears", "smile", "frown", "kiss", "embrace", "touch",
-        "dream", "nightmare", "vision", "hallucination", "illusion", "reality", "truth", "lie", "deception", "betrayal", "loyalty", "honor", "glory", "shame",
-        // Body parts
-        "eye", "lip", "tongue", "tooth", "bone", "skin", "muscle", "vein", "artery", "nerve", "spine", "skull", "brain", "lung", "stomach", "liver", "kidney",
-        "hand", "finger", "thumb", "wrist", "arm", "elbow", "shoulder", "neck", "throat", "chest", "breast", "nipple", "belly", "navel", "hip", "thigh", "knee", "calf", "ankle", "foot", "toe",
-        // Miscellaneous
-        "time", "space", "void", "abyss", "infinity", "eternity", "cosmos", "universe", "galaxy", "star", "planet", "world", "earth", "sun", "moon",
-        "god", "devil", "angel", "demon", "spirit", "phantom", "specter", "wraith", "monster", "beast", "creature", "mutant", "cyborg", "android", "robot",
-        "virus", "malware", "glitch", "bug", "crash", "error", "failure", "success", "victory", "defeat", "triumph", "catastrophe", "disaster", "apocalypse"
-    ];
+const defaultVocab = require('./vocab');
 
-    let vocabConfigList = defaultVocab;
-    // If the .env specifically provides a comma separated list
+app.get('/api/config', (req, res) => {
+    let vocabConfigList = [...defaultVocab]; // Clone to prevent mutation
+
+    // If the .env specifically provides a comma separated list, append it to the pool
     if (process.env.WORD_LIST) {
-        vocabConfigList = process.env.WORD_LIST.split(',').map(s => s.trim()).filter(s => s.length > 0);
+        const customWords = process.env.WORD_LIST.split(',').map(s => s.trim()).filter(s => s.length > 0);
+        vocabConfigList = vocabConfigList.concat(customWords);
     }
 
     res.json({
@@ -54,6 +28,6 @@ app.get('/api/config', (req, res) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Word Claw server listening on port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Word Claw server listening on port ${port}, available on LAN 0.0.0.0`);
 });
---
+++ vocab.js
@@ -0,0 +1,81 @@
+module.exports = [
+    // Core & Connecting Words
+    "the", "a", "an", "and", "but", "or", "so", "yet", "for", "nor", "in", "of", "to", "with", "from", "by", "on", "as", "at", "into",
+    "like", "over", "after", "through", "beneath", "among", "beyond", "upon", "is", "are", "am", "was", "were", "be", "been", "being",
+    "have", "has", "had", "do", "does", "did", "will", "would", "shall", "should", "can", "could", "may", "might", "must",
+    "I", "you", "he", "she", "it", "we", "they", "me", "him", "her", "us", "them", "my", "your", "his", "its", "our", "their", "mine", "yours", "theirs",
+    "this", "that", "these", "those", "who", "whom", "whose", "which", "what", "how", "why", "when", "where",
+    "not", "no", "yes", "always", "never", "forever", "today", "tomorrow", "yesterday", "now", "then", "soon", "late", "early",
+
+    // Cyberpunk & Sci-Fi
+    "neon", "cyber", "synth", "grid", "wire", "chrome", "data", "byte", "glitch", "system", "machine", "ghost", "shell",
+    "terminal", "code", "hacker", "network", "matrix", "nexus", "deck", "jack", "plug", "socket", "circuit", "silicon",
+    "hologram", "laser", "plasma", "static", "noise", "signal", "frequency", "transmission", "broadcast", "channel",
+    "cyborg", "android", "drone", "bot", "algorithm", "protocol", "server", "upload", "download", "stream", "feed",
+    "sprawl", "corporation", "megacorp", "syndicate", "cartel", "mercenary", "credits", "blackmarket", "implant", "upgrade",
+    "overdrive", "bypass", "proxy", "firewall", "encryption", "decryption", "cipher", "key", "access", "denied", "granted",
+    "virtual", "reality", "simulation", "construct", "mainframe", "node", "link", "connection", "offline", "online",
+
+    // Adult, Gritty & Profanity
+    "fuck", "fucking", "fucked", "shit", "bullshit", "damn", "goddamn", "bitch", "cunt", "motherfucker", "asshole", "dick",
+    "pussy", "cock", "slut", "whore", "bastard", "crap", "piss", "prick", "twat", "wanker", "scum", "trash", "filth",
+    "sex", "orgasm", "climax", "penetration", "lust", "desire", "erotic", "kink", "fetish", "bondage", "sadism", "masochism",
+    "dominant", "submissive", "flesh", "naked", "bare", "strip", "pleasure", "pain", "moan", "sweat", "saliva", "breath",
+    "blood", "gore", "guts", "bone", "skull", "skeleton", "corpse", "carcass", "rot", "decay", "maggot", "worm", "disease",
+    "virus", "infection", "toxin", "poison", "venom", "acid", "radioactive", "toxic", "lethal", "fatal", "deadly", "murder",
+    "kill", "destroy", "crush", "smash", "break", "tear", "rip", "shred", "cut", "slice", "stab", "shoot", "bleed", "die",
+
+    // Pop Culture & Tropes
+    "jedi", "sith", "vader", "skywalker", "force", "lightsaber", "empire", "rebellion", "falcon", "star", "trek", "warp",
+    "hobbit", "gandalf", "mordor", "ring", "elf", "dwarf", "orc", "goblin", "dragon", "hogwarts", "potter", "voldemort", "magic",
+    "batman", "joker", "gotham", "superman", "krypton", "mutant", "x-men", "thanos", "avengers", "stark", "wakanda",
+    "targaryen", "lannister", "westeros", "throne", "winter", "terminator", "skynet", "predator", "alien", "ripley",
+    "matrix", "neo", "trinity", "morpheus", "zion", "agent", "smith", "replicant", "blade", "runner", "deckard", "tears",
+
+    // Action Verbs
+    "run", "walk", "crawl", "climb", "fall", "rise", "stand", "sit", "lie", "jump", "leap", "dive", "swim", "fly", "soar",
+    "fight", "punch", "kick", "strike", "hit", "beat", "slap", "grab", "hold", "release", "let", "catch", "throw", "drop",
+    "look", "see", "watch", "gaze", "stare", "glance", "peek", "peep", "glimpse", "blind", "hear", "listen", "sound", "silence",
+    "know", "think", "feel", "believe", "doubt", "hope", "fear", "love", "hate", "want", "need", "wish", "pray", "curse",
+    "eat", "drink", "swallow", "bite", "chew", "taste", "smell", "breathe", "choke", "gag", "cough", "spit", "vomit", "bleed",
+
+    // Descriptive Adjectives
+    "dark", "light", "cold", "hot", "warm", "cool", "deep", "shallow", "high", "low", "far", "near", "wide", "narrow",
+    "long", "short", "heavy", "light", "hard", "soft", "sharp", "dull", "rough", "smooth", "sweet", "sour", "bitter", "salty",
+    "fresh", "stale", "bright", "dim", "faint", "clear", "cloudy", "blind", "deaf", "mute", "old", "young", "new", "ancient",
+    "eternal", "immortal", "mortal", "dead", "alive", "living", "dying", "broken", "fixed", "whole", "shattered", "ruined",
+    "beautiful", "gorgeous", "ugly", "hideous", "vile", "disgusting", "horrific", "terrifying", "scary", "creepy", "spooky",
+    "amazing", "incredible", "awesome", "terrible", "awful", "great", "good", "bad", "evil", "holy", "sacred", "profane",
+
+    // Nouns (Nature, City, Objects)
+    "night", "day", "sun", "moon", "star", "planet", "world", "earth", "sky", "cloud", "rain", "snow", "ice", "frost", "winter",
+    "summer", "spring", "autumn", "wind", "breeze", "storm", "thunder", "lightning", "sea", "ocean", "river", "lake", "water",
+    "fire", "flame", "smoke", "ash", "ember", "spark", "tree", "leaf", "forest", "wood", "stone", "rock", "mountain", "valley",
+    "city", "street", "alley", "building", "tower", "skyscaper", "bridge", "road", "car", "train", "plane", "ship", "boat",
+    "knife", "gun", "sword", "bomb", "bullet", "weapon", "shield", "armor", "helmet", "crown", "ring", "key", "lock", "door",
+    "window", "wall", "floor", "ceiling", "roof", "house", "home", "room", "bed", "chair", "table", "book", "pen", "paper",
+
+    // Body Parts
+    "eye", "lip", "tongue", "tooth", "jaw", "face", "head", "hair", "neck", "throat", "shoulder", "arm", "elbow", "wrist",
+    "hand", "finger", "thumb", "nail", "chest", "breast", "nipple", "belly", "navel", "stomach", "back", "spine", "hip",
+    "pelvis", "groin", "thigh", "knee", "calf", "ankle", "foot", "toe", "heel", "sole", "skin", "muscle", "vein", "artery",
+    "nerve", "brain", "lung", "liver", "kidney", "heart",
+
+    // Poetic (Poe, Dickinson, Whitman, Baudelaire, Bukowski, Plath)
+    "raven", "nevermore", "chamber", "midnight", "dreary", "sorrow", "lenore", "tomb", "sepulchre", "melancholy", "macabre",
+    "soul", "carriage", "immortality", "eternity", "gossamer", "tippet", "tulle", "surmise", "dew", "frost", "meadow", "bliss",
+    "grass", "leaves", "multitudes", "yawp", "barbaric", "kosmos", "democracy", "comrade", "pioneer", "voyage", "electric",
+    "spleen", "ennui", "abyss", "carcass", "fleurs", "mal", "decadence", "absinthe", "opium", "muse", "damned", "vice", "virtue",
+    "booze", "whiskey", "bar", "stool", "neon", "gutter", "poverty", "typewriter", "hangover", "smoke", "cigarette", "cheap",
+    "bell", "jar", "fig", "tree", "oven", "gas", "tulip", "mirror", "moon", "hospital", "madness", "sanity", "suicide", "resurrection",
+    "hollow", "men", "wasteland", "cruelest", "month", "lilacs", "dust", "fear", "handful", "shadow", "red", "rock", "water",
+    "rage", "dying", "light", "gentle", "night", "burn", "rave", "close", "blind", "meteor", "grief", "joy", "ecstasy", "agony",
+
+    // Additional Abstract & Emotional Concepts
+    "time", "space", "void", "infinity", "cosmos", "universe", "galaxy", "dimension", "reality", "illusion", "truth", "lie",
+    "deception", "betrayal", "loyalty", "honor", "glory", "shame", "guilt", "innocence", "sin", "redemption", "salvation",
+    "god", "devil", "angel", "demon", "spirit", "phantom", "specter", "wraith", "monster", "beast", "creature", "mutant",
+    "chaos", "order", "harmony", "discord", "peace", "war", "battle", "conflict", "struggle", "victory", "defeat", "triumph",
+    "catastrophe", "disaster", "apocalypse", "genesis", "revelation", "creation", "destruction", "birth", "death", "rebirth",
+    "silence", "noise", "music", "song", "rhythm", "beat", "melody", "harmony", "chord", "note", "rest", "pause", "crescendo"
+];
